c42ac7bf12b8419cbfe302f11be1b4cfb0832cde
Tobias Allen <ta502@cam.ac.uk>
2022-01-24 16:34:05 +0000

    added demonstration program to task1fF.py

--- Task1F.py
+++ Task1F.py
@@ -1,10 +1,15 @@
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
 
 def run():
     stations = build_station_list()
 
-    for s in stations:
-        print(s.typical_range_consistent())
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+    inconsistent_station_names = []
+    for s in inconsistent_stations:
+        inconsistent_station_names.append(s.name)
+    inconsistent_station_names.sort()
+    print(inconsistent_station_names)
 
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part IA Flood Warning System ***")
