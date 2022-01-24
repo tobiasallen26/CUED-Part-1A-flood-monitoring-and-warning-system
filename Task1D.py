de8e57b6829d72f62e97d511b26a385f8bf6c79b
Tobias Allen <ta502@cam.ac.uk>
2022-01-24 16:06:05 +0000

    added demonstration tasks

--- Task1D.py
+++ Task1D.py
@@ -1,9 +1,18 @@
-from floodsystem.geo import rivers_with_stations
+from floodsystem.geo import rivers_with_stations, stations_by_river
 from floodsystem.stationdata import build_station_list
 
 def run():
     stations = build_station_list()
-    print(rivers_with_stations(stations))
+
+    r_with_s = rivers_with_stations(stations)
+    print('{} stations. First 10 - {}'.format(len(r_with_s), r_with_s[:10]))
+    
+    s_by_r = stations_by_river(stations)
+    s_by_r2 = []
+    for r, s in s_by_r.items():
+        if r in ['River Aire', 'River Cam', 'River Thames']:
+            s.sort()
+            print(r, s, '\n')
 
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part IA Flood Warning System ***")
