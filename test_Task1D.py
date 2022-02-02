c9ff09ca2f2832049280b8d3b4116bfafde36e32
Tobias Allen <ta502@cam.ac.uk>
2022-02-02 18:08:09 +0000

    testing for stations_by_river

--- test_Task1D.py
+++ test_Task1D.py
@@ -1,5 +1,6 @@
-from floodsystem.geo import rivers_with_stations
+from floodsystem.geo import rivers_with_stations, stations_by_river
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
 
 def test_rivers_with_stations():
     """tests requirements of station_by_distance"""
@@ -12,3 +13,14 @@ def test_rivers_with_stations():
     # checks that there are no duplicates in the list
     for river in r_with_s:
         assert r_with_s.count(river) == 1
+
+def test_stations_by_river():
+    """tests requirements of stations_by_river"""
+    stations = build_station_list()
+    s_by_r = stations_by_river(stations)
+    # checks the type of object returned
+    assert type(s_by_r) == dict
+    # checks type of object in s_by_r
+    assert type(s_by_r[s_by_r.values()[0]]) == list
+    # checks the type of object in the lists of stations
+    assert type(s_by_r.items()[0][0]) == MonitoringStation
