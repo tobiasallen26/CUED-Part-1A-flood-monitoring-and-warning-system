c4bead5c6234ac7664ada66b8a72eebbe4aa6752
Tobias Allen <ta502@cam.ac.uk>
2022-02-18 21:16:45 +0000

    corrected error

--- test_Task2B.py
+++ test_Task2B.py
@@ -3,5 +3,5 @@ from floodsystem.station import MonitoringStation
 def test_relative_water_level():
     pass
 
-def test_stations_level_over_threshold(stations, tol):
+def test_stations_level_over_threshold():
     pass
