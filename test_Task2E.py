c11154bf5eb2c01ec81280178b765932a08f6571
Tobias Allen <ta502@cam.ac.uk>
2022-02-28 20:15:16 +0000

    updated test for 2E

--- test_Task2E.py
+++ test_Task2E.py
@@ -5,7 +5,7 @@ import datetime
 def test_plot_water_levels():
     """ tests requirements for plot water levels"""
     # tests by making sure no errors are generated when plot_water_levels is run
-    station = MonitoringStation(None, None, 'Test Plot', None, None, None, None)
+    station = MonitoringStation(None, None, 'Test Plot', None, (3, 7), None, None)
     dates = [datetime.datetime.utcnow() + datetime.timedelta(days=i) for i in range(10)]
     levels = [i for i in range(10)]
     plot_water_levels(station, dates, levels)
