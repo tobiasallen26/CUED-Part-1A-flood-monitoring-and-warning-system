0452b63c3f676bff01d8ab3a586d60f1f6cf61a4
Tobias Allen <ta502@cam.ac.uk>
2022-02-28 20:12:38 +0000

    added plotting of typical range to 2E

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,12 +1,18 @@
 from turtle import window_height, window_width
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_water_levels(station, dates, levels):
     """ plots a graph of water level against dates for a station"""
 
     plt.figure(figsize=(8, 3))
 
+    high_range = np.linspace(station.typical_range[1], station.typical_range[1], len(dates))
+    low_range = np.linspace(station.typical_range[0], station.typical_range[0], len(dates))
+
     plt.plot(dates, levels)
+    plt.plot(dates, high_range)
+    plt.plot(dates, low_range)
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45)
