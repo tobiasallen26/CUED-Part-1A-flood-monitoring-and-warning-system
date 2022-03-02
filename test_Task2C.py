f830af4dda405eb4606f2cff31503f12f6df879d
Aditya Bharadwaj <ab2738@cam.ac.uk>
2022-03-02 13:21:16 +0000

    final

--- test_Task2C.py
+++ test_Task2C.py
@@ -1,8 +1,10 @@
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
 
 def test_2C():
     stations = build_station_list()
+    update_water_levels(stations)
     length = len(stations_highest_rel_level(stations,15))
     assert length==15
     #assert correct length of returned list
