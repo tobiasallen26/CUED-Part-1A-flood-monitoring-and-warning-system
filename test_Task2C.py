9fb74ae1f6c1c0e77be09fe1dfd03a472f891b44
Aditya Bharadwaj <ab2738@cam.ac.uk>
2022-03-02 06:32:44 +0000

    commit on 'main'

---
+++ test_Task2C.py
@@ -0,0 +1,13 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.station import MonitoringStation
+from floodsystem.utils import sorted_by_key
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
+
+
+def test_2C():
+    assert len(stations_highest_rel_level)==15
+    
+
+
