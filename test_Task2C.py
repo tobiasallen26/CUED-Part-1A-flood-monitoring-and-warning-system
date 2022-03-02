71bb4d82fc18a968573daaa91703181e5fadf1ea
Aditya Bharadwaj <ab2738@cam.ac.uk>
2022-03-02 06:46:09 +0000

    should work

--- test_Task2C.py
+++ test_Task2C.py
@@ -4,7 +4,7 @@ from floodsystem.flood import stations_highest_rel_level
 def test_2C():
     stations = []
     length = stations_highest_rel_level(stations,15)
-    assert len(length)==15
+    assert len(length)==0
     #assert correct length of returned list
     
     
